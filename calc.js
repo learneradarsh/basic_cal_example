6bf02ea10e3f175281bd4c1fe6dda9047dde51cf
learneradarsh <learner.adarsh@gmail.com>
2018-11-20 06:09:17 +0530

    code filter done

--- calc.js
+++ calc.js
@@ -1,26 +1,3 @@
-// function c(val) {
-// 	var a1 = $("#bar").val(val);
-// }
-
-// function v(val) {
-// 	var sym = /\.|\+|\-|\*|\//
-//     var barValue = $("#bar").val();
-//     var lastChar = barValue.substring(barValue.length, barValue.length - 1);
-//     if (sym.test(lastChar) && sym.test(val)) {
-//     } else {
-//         var a2 = $("#bar").val($("#bar").val() + val);
-// 	}
-// }
-
-// function equal() {
-//    $("#history").append($("#bar").val()+"=")
-// 	var a3 = c(eval($("#bar").val()))
-// }
-
-// function reset() {
-//     var a4 = $("#bar").val("");
-// }
-
 function c(val) {
     document.getElementById("bar").value = val;
 }
